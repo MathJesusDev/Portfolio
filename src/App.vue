897641119985aed078547d3b1ca009cf71234203
MathJesusDev <matheus.jesus6505@gmail.com>
2025-10-01 17:56:07 -0300

    Adiciona estrutura

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Header from './components/Header.vue';
+
+</script>
+
+<template>
+  <div id="app">
+  <Header />
+  <Hero> </Hero>
+  <Features> </Features>
+  <cta> </Cta>
+  <Footer> </Footer>
+  </div>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,42 @@
+<script lang="ts">
+export default {
+    name: 'Header',
+}
+
+</script>
+
+<template>
+  <header class="header">
+    Minha Marca
+    <div class="contatos">
+        <a href="#hero">Linkedln</a>
+        <a href="#features">Twitter</a>
+        <a href="#cta">Github</a>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+
+header{
+  align-items: center;;
+  display: flex;
+  height: 10%;
+  background: #252525; 
+  color: #f9f9f9; 
+  position: fixed;
+  top: 0;
+  width: 100%;
+  padding: 0 20px;
+  z-index: 1000; 
+}
+
+header .contatos{
+margin-left:auto;
+display: flex;
+gap: 20px;
+}
+
+
+
+</style>
